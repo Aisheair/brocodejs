362fd4273926bb6ad8c428e5d1b4c5c1a2852f4c
Ankit <173899469+Aisheair@users.noreply.github.com>
2024-10-13 14:01:43 +0530

    methode chaining

--- main.js
+++ main.js
@@ -395,20 +395,30 @@ comment
 
 
 //method  chainig = calling onemethod after anothr in one continious line of code 
+// let username = window.prompt("enter your username")
 //-----------------no method chaining-----------------------
 
-let username = window.prompt("enter your username")
-
-username = username.trim()
 
 
+// username = username.trim();
+// let letter = username.charAt(0)
+// letter = letter.toUpperCase()
+
+// let extraChars = username.slice(1)
+// extraChars = extraChars.toLowerCase()
+// username = letter + extraChars
+// console.log(username);
 
 
 
-// ---------------------- method chaining ---------------------
 
 
 
+// // ---------------------- method chaining ---------------------
+
+// username = username.trim().charAt(0).toUpperCase() + username.trim().slice(1).toLocaleLowerCase()
+
+// console.log(username);
 
 
 
