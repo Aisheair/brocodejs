ff4e6795569eccf7f1bbeff3f1529d1ee4b23e4b
Aisheair <ankitkargupta98313@gmail.com>
2024-10-19 17:04:03 +0530

    function in javascript

--- main.js
+++ main.js
@@ -517,41 +517,118 @@ comment
 
 // number gussing game
 
-const minNum = 1;
-const maxNum = 100;
-const answer = Math.floor(Math.random() * (maxNum - minNum + 1) + minNum)
+// const minNum = 1;
+// const maxNum = 100;
+// const answer = Math.floor(Math.random() * (maxNum - minNum + 1) + minNum)
 
-console.log(answer);
+// console.log(answer);
 
-let attemps = 0
-let guess
-let running = true
+// let attemps = 0
+// let guess
+// let running = true
 
-while (running) {
+// while (running) {
 
-   guess =  window.prompt(`guess a number between ${minNum} - ${maxNum}`)
-    guess = Number(guess)
-    if(isNaN(guess)){
-        window.alert(`pls enter a valiv number`)
-    }
-    else if (guess < minNum || guess > maxNum){
-        window.alert(`pls enter a valiv number`)
+//    guess =  window.prompt(`guess a number between ${minNum} - ${maxNum}`)
+//     guess = Number(guess)
+//     if(isNaN(guess)){
+//         window.alert(`pls enter a valiv number`)
+//     }
+//     else if (guess < minNum || guess > maxNum){
+//         window.alert(`pls enter a valiv number`)
+//     }
+//     else{
+//         attemps++
+//         if(guess < answer){
+//             window.alert(`your choice is too low`)
+//         }
+//         else if(guess > answer){
+//             window.alert(`your choice is too high`)
+//         }
+//         else{
+//             window.alert(`correct!!! the answer was ${answer} it took you ${attemps} attemps`)
+//             running = false
+//         }
+//     }
+
+// }
+
+//function = A section of reusable code 
+//           declare code once , use it whenever you want 
+//           call the function to execute that code
+
+// function happyBirthday(username,age){
+//     console.log(`happy birthday toooooooooooo youu`);
+//     console.log(`happy birthday toooooooooooo youu dear`);
+//     console.log(`happy birthday toooooooooooo youu dear ${username}`);
+//     console.log(`happy birthday toooooooooooo youu`);
+//     console.log(`happy birthday toooooooooooo youu dear`);
+//     console.log(`you are ${age} year age`);
+// } 
+
+// happyBirthday("Ankit", 25);
+
+function add(x, y){
+    let result = x + y
+    return result
+}
+function subtract(x,y){
+    return x - y
+}
+function multiply(x,y){
+    return x * y
+}
+function devide(x,y){
+    return x / y
+}
+function isEven(number){
+    return number % 2 === 0 ? true : false
+}
+function isvalidEmail(email){
+    if(email.includes("@")){
+        return true
     }
     else{
-        attemps++
-        if(guess < answer){
-            window.alert(`your choice is too low`)
-        }
-        else if(guess > answer){
-            window.alert(`your choice is too high`)
-        }
-        else{
-            window.alert(`correct!!! the answer was ${answer} it took you ${attemps} attemps`)
-            running = false
-        }
+        return false
     }
-
 }
+
+
+console.log(isvalidEmail("{brofake.com"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
